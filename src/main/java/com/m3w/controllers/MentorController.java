6e12798830ec6dcf2dbd39954e18c51874c3a0d1
mikolaj <mikolaj.urbanek@gmail.com>
2020-06-03 12:38:32 +0200

    mentor has power to delete students and update their data

--- src/main/java/com/m3w/controllers/MentorController.java
+++ src/main/java/com/m3w/controllers/MentorController.java
@@ -1,6 +1,6 @@
 package com.m3w.controllers;
 
-import com.m3w.dao.StudentDao;
+import com.m3w.dao.MentorDao;
 import com.m3w.models.Student;
 
 import java.util.List;
@@ -38,14 +38,16 @@ public class MentorController {
                 case 7:
                     updateStudentData();
                     break;
+                case 0:
+                    isRun = false;
 
             }
         }
     }
 
     private void getListOfStudents() {
-        StudentDao studentdao = new StudentDao();
-        List<Student> students = studentdao.getStudentsDetail();
+        MentorDao mentorDao = new MentorDao();
+        List<Student> students = mentorDao.getStudentsDetail();
         for (Student s: students){
             System.out.println("[" + s.getId() +"]  "+ s.getName() +" "+ s.getSurname()  + "  |Phone number: " + s.getPhone() + " |E-mail: " + s.getEmail());
         }
@@ -61,7 +63,7 @@ public class MentorController {
     }
 
     private void addStudent() {
-             StudentDao studentdao = new StudentDao();
+            MentorDao mentorDao = new MentorDao();
             System.out.println("Provide name of the new student: ");
             String newName = input.next();
             System.out.println("Provide surname of the new student: ");
@@ -73,15 +75,59 @@ public class MentorController {
             System.out.println("Provide his password: ");
             String newPassword = input.next();
 
-            studentdao.createStudent(newName, newSurname, newPhone, newEmail, newPassword, "student");
+            mentorDao.createStudent(newName, newSurname, newPhone, newEmail, newPassword, "student");
     }
 
+   public void removeStudent() {
+        MentorDao mentorDao = new MentorDao();
+        getListOfStudents();
+        System.out.println("Which student do You want to delete? (via E-mail) ");
+        String email = input.next();
+        mentorDao.deleteStudent(email);
 
-    private void removeStudent() {
     }
 
     private void updateStudentData() {
-    }
+        MentorDao mentorDao = new MentorDao();
+        getListOfStudents();
+        System.out.println("Which student do You want to change details? ");
+        String email = input.next();
+        System.out.println("Which data do You want to change? " +
+                "\n[1] Name" +
+                "\n[2] Surname" +
+                "\n[3] Phone number" +
+                "\n[4] E-mail adress");
 
+        boolean isRunning = true;
+        while (isRunning){
+        int userChoice = input.nextInt();
+        switch(userChoice){
+            case 1:
+                System.out.println("Provide new name for the student: ");
+                String newName = input.next();
+                mentorDao.updateStudentDataString("name", newName, email);
+                break;
+            case 2:
+                System.out.println("Provide new surname for the student: ");
+                String newSurname = input.next();
+                mentorDao.updateStudentDataString("surname", newSurname, email);
+                break;
+            case 3:
+                System.out.println("Provide student's new phone number: ");
+                int newPhone = input.nextInt();
+                mentorDao.updateStudentDataInt("phone", newPhone, email);
+                break;
+            case 4:
+                System.out.println("Provide student's new E-mail adress:  ");
+                String newEmail = input.next();
+                mentorDao.updateStudentDataString("email", newEmail, email);
+                break;
+            case 0:
+                isRunning = false;
+           }
+
+        }
+
+    }
 
 }
