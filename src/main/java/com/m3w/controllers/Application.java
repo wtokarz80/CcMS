18bb1290a7b526cd9d4b60b2454116fb6bb66cca
Mat191126 <matprz11@gmail.com>
2020-06-03 19:18:04 +0200

    Add Attendance class

--- src/main/java/com/m3w/controllers/Application.java
+++ src/main/java/com/m3w/controllers/Application.java
@@ -1,20 +1,11 @@
 package com.m3w.controllers;
 
-import com.m3w.dao.LoginDao;
-import com.m3w.models.User;
-
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Application {
 
-
-
-
     public static void main(String[] args) throws IOException {
-
         MenuController menuController = new MenuController();
-
         menuController.mainMenu();
     }
 }
