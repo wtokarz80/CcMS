d5a69aab4d2e6daa80724e924fde0ece7e0133a6
Mat191126 <matprz11@gmail.com>
2020-06-04 11:22:25 +0200

    Add EmployeeDAO with selectAllStudents method, add method showing all students in EmployeeController

--- src/main/java/com/m3w/controllers/EmployeeController.java
+++ src/main/java/com/m3w/controllers/EmployeeController.java
@@ -1,18 +1,22 @@
 package com.m3w.controllers;
 
+import com.m3w.dao.EmployeeDAO;
 import com.m3w.models.Employee;
+import com.m3w.models.Student;
 import com.m3w.services.InputProvider;
 import com.m3w.view.DataPrinting;
 import com.m3w.view.MenuPrinting;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeeController {
 
     private InputProvider inputProvider = new InputProvider();
     private MenuPrinting menuPrinting = new MenuPrinting();
     private DataPrinting dataPrinting = new DataPrinting();
-    //private EmployeeDAO = new EmployeeDAO();
+    private EmployeeDAO employeeDAO = new EmployeeDAO();
     private Employee employee;
 
     public EmployeeController(Employee employee) {
@@ -26,7 +30,7 @@ public class EmployeeController {
         switch (userChoice) {
             case 1:
                 System.out.println("\nShow list of students\n");
-                //showAllStudents();
+                showAllStudents();
                 break;
             case 0:
                 System.out.println("Back to previous menu");
@@ -34,6 +38,15 @@ public class EmployeeController {
             default:
                 employeeOptions();
                 break;
+        }
+    }
+
+
+    private void showAllStudents() {
+        List<Student> studentList = new ArrayList<>();
+        studentList = employeeDAO.getStudentDetail();
+        for (Student student : studentList){
+            System.out.println("[" + student.getId() +"]  "+ student.getName() +" "+ student.getSurname()  + "  |Phone number: " + student.getPhone() + " |E-mail: " + student.getEmail());
         }
     }
 }
